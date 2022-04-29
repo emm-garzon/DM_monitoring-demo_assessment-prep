e72d852ced9f58807c6adb91b46fcae988b76eea
Emmanuel Garzon <eggarzon@gmail.com>
2022-04-28 23:04:41 -0700

    updated app.get, errorHandler

--- server/index.js
+++ server/index.js
@@ -18,12 +18,30 @@ let rollbar = new Rollbar({
 // record a generic message and send it to Rollbar
 rollbar.log("Hello world!");
 
+let students = [];
+
+app.post("/api/student", (req, res) => {
+  let { name } = req.body;
+  name = name.trim();
+
+  students.push(name);
+
+  rollbar.log("student name successfully added!", {
+    author: "Scott",
+    type: "manual entry",
+  });
+
+  res.status(200).send(students);
+});
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
   rollbar.info("html file served successfully.");
 });
 
 const port = process.env.PORT || 4545;
+
+app.use(rollbar.errorHandler());
 
 app.listen(port, () => {
   console.log(`Take us to warp ${port}`);
