53f7c664d2115b08625be43f8051954908120d29
Emmanuel Garzon <eggarzon@gmail.com>
2022-04-28 22:11:43 -0700

    added rollbar

--- server/index.js
+++ server/index.js
@@ -7,6 +7,17 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: "a5e4e0eaed484191abc219191bbd8fc5",
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
+
 const port = process.env.PORT || 4545;
 
 app.listen(port, () => {
